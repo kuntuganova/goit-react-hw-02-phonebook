81db33e915a5af0620b9c16aa874fa1046dbc6b2
kuntuganova <elya.kuntuganova@gmail.com>
2023-05-12 15:11:56 +0600

    Contact component added

--- src/components/contactList/ContactList.jsx
+++ src/components/contactList/ContactList.jsx
@@ -1,47 +1,36 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import css from './ContactList.module.css'
-
-
+import css from './ContactList.module.css';
+import Contact from 'components/contact/Contact';
 
 class ContactList extends Component {
-
-    
-    
   render() {
-      return (
+    return (
       <ul className={css.ul}>
-        {this.props.contacts.map(({ id, name, number }) => {
-          return (
-            <li className={css.li} key={id}>
-              <p className={css.p}>
-                {name}: {number}
-              </p>
-              <button
-                className={css.btn}
-                onClick={() => {
-                  this.props.deleteContact(id);
-                }}
-              >
-                Delete
-              </button>
-            </li>
-          );
-        })}
+        {this.props.contacts.map(({ id, name, number }) => (
+          <Contact
+            key={id}
+            id={id}
+            name={name}
+            number={number}
+            onDeleteContact={this.props.deleteContact}
+          />
+        ))}
       </ul>
     );
   }
 }
 
 ContactList.propTypes = {
-    contacts: PropTypes.arrayOf(PropTypes.shape({
-        id: PropTypes.string.isRequired,
-        name: PropTypes.string.isRequired,
-        number: PropTypes.string.isRequired,
-    })),
+  contacts: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+      number: PropTypes.string.isRequired,
+    })
+  ),
   deleteContact: PropTypes.func.isRequired,
   filteredName: PropTypes.string.isRequired,
-        }
-
+};
 
 export default ContactList;
